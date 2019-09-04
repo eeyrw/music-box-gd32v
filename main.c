c5629a7f15ff3abb0084b6f13594d22449149763
yuan <ayrw@163.com>
2019-09-04 18:50:48 +0800

    M

--- main.c
+++ main.c
@@ -40,6 +40,15 @@ void gpio_config(void);
 /* configure the TIMER peripheral */
 void timer_config(void);
 
+void TIMER1_IRQHandler(void)
+{
+    if(SET == timer_interrupt_flag_get(TIMER1, TIMER_INT_CH0)){
+        /* clear channel 0 interrupt bit */
+        timer_interrupt_flag_clear(TIMER1, TIMER_INT_CH0);
+    }
+}
+
+
 /**
     \brief      configure the GPIO ports
     \param[in]  none
@@ -90,10 +99,10 @@ void timer_config(void)
     /* initialize TIMER init parameter struct */
     timer_struct_para_init(&timer_initpara);
     /* TIMER0 configuration */
-    timer_initpara.prescaler         = 5399;
+    timer_initpara.prescaler         = 0;
     timer_initpara.alignedmode       = TIMER_COUNTER_EDGE;
     timer_initpara.counterdirection  = TIMER_COUNTER_UP;
-    timer_initpara.period            = 15999;
+    timer_initpara.period            = 1023;
     timer_initpara.clockdivision     = TIMER_CKDIV_DIV1;
     timer_initpara.repetitioncounter = 0;
     timer_init(TIMER0, &timer_initpara);
@@ -129,14 +138,92 @@ void timer_config(void)
     /* auto-reload preload enable */
     timer_auto_reload_shadow_enable(TIMER0);
     timer_enable(TIMER0);
+
+
+    /* ----------------------------------------------------------------------------
+    TIMER1 Configuration:
+    TIMER1CLK = SystemCoreClock/5400 = 20KHz.
+    TIMER1 configuration is timing mode, and the timing is 0.2s(4000/20000 = 0.2s).
+    CH0 update rate = TIMER1 counter clock/CH0CV = 20000/4000 = 5Hz.
+    ---------------------------------------------------------------------------- */
+    timer_oc_parameter_struct timer1_ocinitpara;
+    timer_parameter_struct timer1_initpara;
+
+    rcu_periph_clock_enable(RCU_TIMER1);
+
+    timer_deinit(TIMER1);
+    /* initialize TIMER init parameter struct */
+    timer_struct_para_init(&timer_initpara);
+    /* TIMER1 configuration */
+    timer1_initpara.prescaler         = 5399;
+    timer1_initpara.alignedmode       = TIMER_COUNTER_EDGE;
+    timer1_initpara.counterdirection  = TIMER_COUNTER_UP;
+    timer1_initpara.period            = 4000;
+    timer1_initpara.clockdivision     = TIMER_CKDIV_DIV1;
+    timer_init(TIMER1, &timer1_initpara);
+
+    /* initialize TIMER channel output parameter struct */
+    timer_channel_output_struct_para_init(&timer_ocinitpara);
+    /* CH0,CH1 and CH2 configuration in OC timing mode */
+    timer1_ocinitpara.outputstate  = TIMER_CCX_ENABLE;
+    timer1_ocinitpara.ocpolarity   = TIMER_OC_POLARITY_HIGH;
+    timer1_ocinitpara.ocidlestate  = TIMER_OC_IDLE_STATE_LOW;
+    timer_channel_output_config(TIMER1, TIMER_CH_0, &timer1_ocinitpara);
+
+    /* CH0 configuration in OC timing mode */
+    timer_channel_output_pulse_value_config(TIMER1, TIMER_CH_0, 2000);
+    timer_channel_output_mode_config(TIMER1, TIMER_CH_0, TIMER_OC_MODE_TIMING);
+    timer_channel_output_shadow_config(TIMER1, TIMER_CH_0, TIMER_OC_SHADOW_DISABLE);
+
+    timer_interrupt_enable(TIMER1, TIMER_INT_CH0);
+    timer_enable(TIMER1);
 }
 
+void uart_config(void)
+{
+    /* enable GPIO clock */
+    rcu_periph_clock_enable(RCU_GPIOA);
+
+    /* enable USART clock */
+    rcu_periph_clock_enable(RCU_USART0);
+
+    /* connect port to USARTx_Tx */
+    gpio_init(GPIOA, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_9);
+
+    /* connect port to USARTx_Rx */
+    gpio_init(GPIOA, GPIO_MODE_IN_FLOATING, GPIO_OSPEED_50MHZ, GPIO_PIN_10);
+
+    /* USART configure */
+    usart_deinit(USART0);
+    usart_baudrate_set(USART0, 115200U);
+    usart_word_length_set(USART0, USART_WL_8BIT);
+    usart_stop_bit_set(USART0, USART_STB_1BIT);
+    usart_parity_config(USART0, USART_PM_NONE);
+    usart_hardware_flow_rts_config(USART0, USART_RTS_DISABLE);
+    usart_hardware_flow_cts_config(USART0, USART_CTS_DISABLE);
+    usart_receive_config(USART0, USART_RECEIVE_ENABLE);
+    usart_transmit_config(USART0, USART_TRANSMIT_ENABLE);
+    usart_enable(USART0);
+}
+
+/* retarget the C library printf function to the USART */
+int _put_char(int ch)
+{
+    usart_data_transmit(USART0, (uint8_t) ch );
+    while ( usart_flag_get(USART0, USART_FLAG_TBE)== RESET){
+    }
+
+    return ch;
+}
 
 extern void TestInit(void);
 extern void TestProcess(void);
 int main(void)
 {
     gpio_config(); 
+    eclic_global_interrupt_enable();
+    eclic_set_nlbits(ECLIC_GROUP_LEVEL3_PRIO1);
+    eclic_irq_enable(TIMER1_IRQn,1,0);
     timer_config();
     TestInit();
     TestProcess();
